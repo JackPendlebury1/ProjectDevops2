b20c8178505d9e379006f9e6cd82b2d6a2c683c9
neinomas <neinomas@gmail.com>
2021-02-21 14:30:51 +0000

    hard coding for testing

--- service-1-frontend/app/__init__.py
+++ service-1-frontend/app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__,template_folder='../templates')
-app.config['SECRET_KEY'] = getenv('SECRET')
+app.config['SECRET_KEY'] = "getenv('SECRET')"
 
 from app import routes
