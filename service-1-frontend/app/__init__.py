417ee5947c231e15c08b3779df0be130c6f7314f
neinomas <neinomas@gmail.com>
2021-02-21 19:38:35 +0000

    added database to flask app

--- service-1-frontend/app/__init__.py
+++ service-1-frontend/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder='../templates')
-app.config['SECRET_KEY'] = "getenv('SECRET')"
+app.config["SQLALCHEMY_DATABASE_URI"] =  getenv("DATABASE_URI")
+app.config['SECRET_KEY'] = getenv('SECRET')
+db = SQLAlchemy(app)
 
 from app import routes
