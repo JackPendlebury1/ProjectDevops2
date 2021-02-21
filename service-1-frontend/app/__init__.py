99e95b71e3d9a9a6989c1870076a7d47844b599f
neinomas <neinomas@gmail.com>
2021-02-21 20:11:19 +0000

    updated a few files

--- service-1-frontend/app/__init__.py
+++ service-1-frontend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder='../templates')
 app.config["SQLALCHEMY_DATABASE_URI"] =  getenv("DATABASE_URI")
-app.config['SECRET_KEY'] = getenv('SECRET')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from app import routes
